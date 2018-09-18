b7819ee2b495aaec04a99e83c9a391ce6b14cebf
Anthony Tiongson <anthonytiongson@me.com>
2018-09-17 20:38:09 -0400

    pages, getPages
    pages integer field added as well as a getPages accessor method

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Add the methods here ...
@@ -34,6 +36,12 @@ class Book
     public String getTitle()
     {
         return title;
+    }
+    
+    // Accessor for a book's pages
+    public int getPages()
+    {
+        return pages;
     }
     
     // Method to print a book's author to terminal
